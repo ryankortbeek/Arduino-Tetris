12eb5d58998fcfe3c74ffd419d4c4fa15cf3924f
Ryan Kortbeek <43446631+ryankortbeek@users.noreply.github.com>
2019-07-04 15:22:59 -0600

    Update play.h

--- play.h
+++ play.h
@@ -1,13 +1,3 @@
-/*------------------------------------------------
-#   Name: Justin Boileau, Ryan Kortbeek
-#   ID: 1535268, 1535901
-#   CMPUT 275, Winter 2019
-#
-#   Tetris: play.h
-#
-#   Header file for play.cpp
-#
-#-----------------------------------------------*/
 #ifndef __CONSTS_AND_TYPES_H
 #define __CONSTS_AND_TYPES_H
 
